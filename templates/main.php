6291b828fd6a413fb270f19849c1dad4ed4e400e
Aiganym33 <63113784+Aiganym33@users.noreply.github.com>
2020-04-03 21:39:01 +0600

    Add files via upload

---
+++ templates/main.php
@@ -0,0 +1,11 @@
+div id="promo">
+        <h1 id="promoText">
+            Новый PhoneBrand - это лучший смартфон
+        </h1>
+    </div>
+
+    <div id="mainTextWrap">
+        <div id="mainText">
+        If as increasing contrasted entreaties be. Polite do object at passed it is. Whatever throwing we on resolved entrance together graceful. We leaf to snug on no need. So by colonel hearted ferrars. Is inquiry no he several excited am. Fat new smallness few supposing suspicion two. Now summer who day l
+        </div>
+    </div>
